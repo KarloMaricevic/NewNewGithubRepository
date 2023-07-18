38415f67c2e08b85a0b0b04673bbe9a6a683ba88
Karlo Maricevic <karlo.maricevic@undabot.com>
2023-07-18 14:11:11 +0200

    Add compose to project
    + bump kotlin version so its compatible with latest compose

--- app/src/main/java/com/example/githubapp/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/MainActivity.kt
@@ -1,8 +1,36 @@
 package com.example.githubapp
 
+import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { MainScreen() }
+    }
+}
 
+@Composable
+private fun MainScreen() {
+    Box(Modifier.fillMaxSize()) {
+        Text(
+            text = "Hello!",
+            modifier = Modifier.align(Alignment.Center),
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun MainScreenPreview() {
+    MainScreen()
 }
