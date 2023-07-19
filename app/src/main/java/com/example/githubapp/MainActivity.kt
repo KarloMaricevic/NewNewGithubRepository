52d7c23d9550bc936ce00e488fbff9bb21ebdc45
Karlo Maricevic <karlo.maricevic@undabot.com>
2023-07-19 11:42:12 +0200

    Use githubapp navigation in app

--- app/src/main/java/com/example/githubapp/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/MainActivity.kt
@@ -3,40 +3,23 @@ package com.example.githubapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.githubapp.core.components.GitHubAppNavigation
+import com.example.githubapp.core.navigation.Navigator
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var navigator: Navigator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            GitHubAppNavigation(navigator)
         }
     }
-}
-
-@Composable
-private fun MainScreen() {
-    Box(Modifier.fillMaxSize()) {
-        Text(
-            text = "Hello!",
-            modifier = Modifier.align(Alignment.Center),
-        )
-    }
-}
-
-@Preview
-@Composable
-private fun MainScreenPreview() {
-    MainScreen()
 }
