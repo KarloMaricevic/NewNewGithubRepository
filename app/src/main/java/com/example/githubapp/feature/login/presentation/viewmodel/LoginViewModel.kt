23a5405dc143f8cab450537b01db476668ab8d99
Karlo Maricevic <karlo.maricevic@undabot.com>
2023-07-19 23:43:32 +0200

    Implement basic login view model

--- app/src/main/java/com/example/githubapp/feature/login/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/githubapp/feature/login/presentation/viewmodel/LoginViewModel.kt
@@ -1,12 +1,41 @@
 package com.example.githubapp.feature.login.presentation.viewmodel
 
+import androidx.lifecycle.viewModelScope
 import com.example.githubapp.core.viewmodel.BaseViewModel
 import com.example.githubapp.feature.login.presentation.model.LoginEvent
+import com.example.githubapp.feature.login.presentation.model.LoginEvent.OnLoginClicked
+import com.example.githubapp.feature.login.presentation.model.LoginEvent.OnPasswordInputChanged
+import com.example.githubapp.feature.login.presentation.model.LoginEvent.OnUsernameInputChanged
+import com.example.githubapp.feature.login.presentation.model.LoginViewState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
-class LoginViewModel @Inject constructor(): BaseViewModel<LoginEvent>() {
+class LoginViewModel @Inject constructor() : BaseViewModel<LoginEvent>() {
+
+    private val _viewState = MutableStateFlow(LoginViewState())
+    val viewState = _viewState.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = LoginViewState(),
+    )
 
     override fun onEvent(event: LoginEvent) {
-        TODO("Not yet implemented")
+        when (event) {
+            is OnUsernameInputChanged -> _viewState.update { viewState ->
+                viewState.copy(
+                    username = event.input
+                )
+            }
+            is OnPasswordInputChanged -> _viewState.update { viewState ->
+                viewState.copy(
+                    password = event.input
+                )
+            }
+            OnLoginClicked -> {  /*TODO*/
+            }
+        }
     }
 }
