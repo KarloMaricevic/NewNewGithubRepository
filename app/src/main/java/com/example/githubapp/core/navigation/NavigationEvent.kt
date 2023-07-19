26f283e5cd8646576058952a0dbf406d8bf44189
Karlo Maricevic <karlo.maricevic@undabot.com>
2023-07-19 10:27:49 +0200

    Add mechanism for navigation

---
+++ app/src/main/java/com/example/githubapp/core/navigation/NavigationEvent.kt
@@ -0,0 +1,12 @@
+package com.example.githubapp.core.navigation
+
+import androidx.navigation.NavOptionsBuilder
+
+sealed interface NavigationEvent {
+    object NavigateUp : NavigationEvent
+    object NavigateBack : NavigationEvent
+    data class Destination(
+        val destination: String,
+        val builder: NavOptionsBuilder.() -> Unit = {},
+    ) : NavigationEvent
+}
